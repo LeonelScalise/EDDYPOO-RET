02954e7f9991f053f75e43ebfff7a1b09d272a2b
LeonelScalise <lscalise@itba.edu.ar>
2023-04-23 03:39:32 -0300

    cambio de clear

--- Proyecto/clasePersona.py
+++ Proyecto/clasePersona.py
@@ -17,10 +17,8 @@ class Alumno(Persona):
 
   def menu_registro_alumno(institucion:Institucion):
     x = "o"
-    clear()
-
     legajo_ingresado = validadorLegajo(institucion)
-
+    clear()
     for alumno in institucion.alumnos:
         if alumno.legajo == legajo_ingresado:
           if alumno.sexo == "F":
@@ -89,7 +87,7 @@ class Administrativo(Persona):
 
   def menu_registro_administrativo(institucion:Institucion):
     x = "o"
-    legajo_ingresado = int(input("Ingrese su numero de legajo: "))
+    legajo_ingresado = validadorLegajo(institucion)
     clear()
     for admin in institucion.administrativos:
         if admin.legajo == legajo_ingresado:
